908d9479973f5a7962f8a29dd9f3cfb72d3543db
Shelven Zhou <favoring.zhou49@gmail.com>
2026-04-14 14:07:37 +0000

    project init

---
+++ src/.config/user.ts
@@ -0,0 +1,23 @@
+import type { UserConfig } from '~/types'
+
+export const userConfig: Partial<UserConfig> = {
+  site: {
+    title: '集异璧之大成',
+    subtitle: 'by Shelven Zhou',
+    author: 'Shelven Zhou',
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/shelvenzhou',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      {
+        name: 'email',
+        href: 'mailto:favoring.zhou49@gmail.com',
+      },
+    ],
+  },
+}
